14157ef2efa8e19a665ac2ea5656daed107f6f5e
Upendra <ashu23081991@gmail.com>
2016-09-23 12:43:50 +0530

    Create routing with the UI-Router module

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
+var app = angular.module("myApp", ["ui.router"]);
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+	$stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('about', {
+            url: '/about',
+            templateUrl : "views/about.html",
+			controller : 'about-controller'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('faq', {
+            // we'll get to this in a bit  
+			url: '/faq',
+			templateUrl : "views/faq.html",
+			controller : 'faq-controller'			
+        })
+		
+		.state('contact', {
+            // we'll get to this in a bit  
+			url: '/contact',
+			templateUrl : "views/contact.html",
+			controller : 'contact-controller'			
+        })
+		
+		.state('last', {
+            // we'll get to this in a bit  
+			url: '/last',
+			templateUrl : "views/last.html",
+			controller : 'last-controller'			
+        });
+	/*
     .when("/", {
         templateUrl : "views/about.html",
 		controller : 'about-controller'
@@ -16,5 +47,12 @@ app.config(function($routeProvider) {
 	
     .when("/last", {
         templateUrl : "views/last.html"
-    });
+    });*/
+});
+
+
+app.directive('myCustomer', function() {
+  return {
+    template: 'hgdskhjg jhvw fgliuwg '
+  };
 });
--- controllers/faq-controller.js
+++ controllers/faq-controller.js
@@ -1,3 +1,15 @@
 app.controller('faq-controller', function($scope){
 	$scope.msg = "I have new faq controller";
+	$scope.number = {
+               
+               summation: function() {
+                  var sum;
+                  sum = $scope.number;
+                  return parseInt(sum.first) + parseInt(sum.last);
+               }
+            };
+			
+	$scope.checking = function(){
+		alert($scope.user);
+	};
 })
