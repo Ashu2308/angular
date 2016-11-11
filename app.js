ec7c25d53acf7aba0c3b5bae8d43c2cf9c75539b
UpendraTech <upendra.khare@advaitsolutions.in>
2016-11-11 22:25:29 +0530

    commit

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
 var app = angular.module("myApp", ["ui.router"]);
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-	$stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
+    $stateProvider
+    // HOME STATES AND NESTED VIEWS ========================================
         .state('about', {
-            url: '/about',
-            templateUrl : "views/about.html",
-			controller : 'about-controller'
+            url: '/about'
+            , templateUrl: "views/about.html"
+            , controller: 'about-controller'
         })
-        
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('faq', {
             // we'll get to this in a bit  
-			url: '/faq',
-			templateUrl : "views/faq.html",
-			controller : 'faq-controller'			
-        })
-		
-		.state('contact', {
+            url: '/faq'
+            , templateUrl: "views/faq.html"
+            , controller: 'faq-controller'
+        }).state('contact', {
             // we'll get to this in a bit  
-			url: '/contact',
-			templateUrl : "views/contact.html",
-			controller : 'contact-controller'			
-        })
-		
-		.state('last', {
+            url: '/contact'
+            , templateUrl: "views/contact.html"
+            , controller: 'contact-controller'
+        }).state('last', {
             // we'll get to this in a bit  
-			url: '/last',
-			templateUrl : "views/last.html",
-			controller : 'last-controller'			
+            url: '/last'
+            , templateUrl: "views/last.html"
+            , controller: 'last-controller'
         });
-	/*
+    /*
     .when("/", {
         templateUrl : "views/about.html",
 		controller : 'about-controller'
@@ -49,10 +43,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
         templateUrl : "views/last.html"
     });*/
 });
-
-
-app.directive('myCustomer', function() {
-  return {
-    template: 'hgdskhjg jhvw fgliuwg '
-  };
+app.directive('myCustomer', function () {
+    return {
+        template: 'hgdskhjg jhvw fgliuwg '
+    };
 });
